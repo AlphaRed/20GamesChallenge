7cc6c8c9ce5fc0efe7ef1c3fa9bed07d62883d93
Isaac Volk <isaacvolk3@gmail.com>
2024-08-15 22:16:14 -0400

    Added max velocity to paddle

--- Game2/paddle.gd
+++ Game2/paddle.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+const MAX_VEL = 150
+
 func _ready():
 	pass # Replace with function body.
 
@@ -8,6 +10,10 @@ func _process(_delta):
 		velocity.x -= 10
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 10
-
+	
 func _physics_process(_delta):
+	if velocity.x > MAX_VEL:
+		velocity.x = MAX_VEL
+	if velocity.x < -MAX_VEL:
+		velocity.x = -MAX_VEL
 	move_and_slide()
